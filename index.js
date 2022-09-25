0b1cfdcf798077d13a87ed98965141b02490099a
Shreyaan <Shreyaans41@gmail.com>
2022-09-25 11:34:21 +0530

    dummy text

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+if (localStorage.getItem("notes") == null||undefined ) {
+  noteObj = {
+    title: 'Markdown Preview',
+    body: MarkdownPreview(),
+  };
+  let mainNotesArray=[]
+  mainNotesArray.push(noteObj);
+  localStorage.setItem("notes", JSON.stringify(mainNotesArray));
+}
+
 showAllNotes();
+
+
 
 let boldBtn = document.getElementById("bold_button");
 
@@ -244,4 +256,57 @@ function parseMd(md) {
   md = md.replace(/(\<pre.+\>)\s*\n\<p\>(.+)\<\/p\>/gm, "$1$2");
 
   return md;
+}
+
+function MarkdownPreview(){
+  return `
+  # Markdown syntax guide
+
+## Headers 
+ 
+# This is a Heading h1
+## This is a Heading h2 
+###### This is a Heading h6
+
+## Emphasis
+
+*This text will be italic*  
+_This will also be italic_
+
+**This text will be bold**  
+__This will also be bold__
+
+_You **can** combine them_
+
+## Lists
+
+### Unordered
+
+* Item 1
+* Item 2
+* Item 2a
+* Item 2b
+
+### Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+  1. Item 3a
+  1. Item 3b
+
+## Images
+
+![This is an alt text.](https://picsum.photos/200/300 "This is a sample image.")
+
+## Links
+[Go back to main page](./)
+
+## Blockquotes
+
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
+>
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+
+  `
 }
